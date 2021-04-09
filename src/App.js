f233c69632ac103c3a71a9feb29a55baf0e7ca82
Nika <delmare@mail.ru>
2021-04-09 11:15:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,129 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import { useTable } from 'react-table';
+
+function Table({columns, data}) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({
+    columns,
+    data
+  })
+  return(
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>
+                {column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>
+                  {cell.render('Cell')}
+                </td>
+              })}
+              <input type= 'submit' name= 'type' />
+              <input type= 'submit' name= 'type' />
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
 function App() {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: ' ',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'firstName'
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'lastName'
+          },
+          {
+            Header: 'user',
+            accessor: 'user'
+          },
+          {
+            Header: 'mail',
+            accessor: 'mail'
+          },
+          {
+            Header: 'Login',
+            accessor: 'login'
+          }
+        ],
+      },
+    ]
+  )
+
+  const data = React.useMemo(() => [
+    {
+      firstName: 'firstName',
+      lastName: 'lastName',
+      user: 'user',
+      mail: 'mail',
+      login: 'login',
+    },
+    {
+      firstName: 'firstName',
+      lastName: 'lastName',
+      user: 'user',
+      mail: 'mail',
+      login: 'login'
+    },
+    {
+      firstName: 'firstName',
+      lastName: 'lastName',
+      user: 'user',
+      mail: 'mail',
+      login: 'login'
+    },
+    {
+      firstName: 'firstName',
+      lastName: 'lastName',
+      user: 'user',
+      mail: 'mail',
+      login: 'login'
+    },
+    {
+      firstName: 'firstName',
+      lastName: 'lastName',
+      user: 'user',
+      mail: 'mail',
+      login: 'login'
+    },
+    {
+      firstName: 'firstName',
+      lastName: 'lastName',
+      user: 'user',
+      mail: 'mail',
+      login: 'login'
+    }
+], [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table columns={columns} data={data} />
     </div>
   );
 }
